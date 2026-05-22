c4a557d94ae2a9e00798a6fd2ba3d7258aa776a8
Alex Kalkatos <alexcsts@gmail.com>
2026-05-22 11:33:06 -0300

    Initial commit

---
+++ SceneController/scene_loader.gd
@@ -0,0 +1,13 @@
+class_name SceneLoader
+## Simple utility component to trigger scene transitions to a pre-defined TPCN file.
+extends Node
+
+@export_file("*.tscn") var scene: String
+
+
+## Changes the current scene to the one specified in the 'scene' export property.
+func load_scene () -> void:
+	if !scene:
+		push_error("No scene provided.")
+		return
+	get_tree().change_scene_to_file(scene)
